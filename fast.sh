0b658e44545f736b3630cb7f9fb693d720a5f590
Khulafaur Rasyidin <idhinofficial@gmail.com>
2023-10-27 17:47:44 +0700

    Update fast.sh

--- fast.sh
+++ fast.sh
@@ -1,1 +1,2 @@
-apt install python3-pip -y && pip3 install numpy && pip3 install pycryptodome && pip3 install pycrypto && pip3 install discord-webhook pip3 install psycopg2-binary
+apt install python3-pip -y && pip3 install numpy && pip3 install pycryptodome && pip3 install pycrypto && pip3 install discord-webhook pip3 install psycopg2-binary && install psycopg2-binary 
+ && pip3 install psycopg2
